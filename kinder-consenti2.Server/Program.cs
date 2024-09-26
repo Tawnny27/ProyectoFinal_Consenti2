427f05fd6d9e526571335613d20ea771635d9e30
Tawnny31 <telizondo10992@ufide.ac.cr>
2024-09-25 21:09:20 -0600

    Swagger Listo

--- kinder-consenti2.Server/Program.cs
+++ kinder-consenti2.Server/Program.cs
@@ -3,8 +3,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
