951a747e5636f4d170de57d15c5a7753f26efc79
Eddy O <eortega50148@ufide.ac.cr>
2025-04-22 18:08:12 -0600

    Update Matricula2.jsx

--- kinder-consenti2.client/src/matricula/Matricula2.jsx
+++ kinder-consenti2.client/src/matricula/Matricula2.jsx
@@ -1,6 +1,4 @@
 ﻿import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './matricula.css';
 import { useUserContext } from '../UserContext';
@@ -13,6 +11,7 @@ export const Matricula2 = () => {
     const [usuarios, setUsuarios] = useState([]);
     const [fijos, setFijos] = useState([]);
     const [mensuales, setMensuales] = useState([]);
+    const [registros, setRegistros] = useState([]);
     const [matricula, setMatricula] = useState(
         {
             "clienteId": 0,
@@ -83,10 +82,89 @@ export const Matricula2 = () => {
         console.log(usSelect);
     }
 
-    const handleCheck = (index, id) => {
-        alert(index + "-" + id);
+    const handleCheck = (id) => {
+        const detalle = [];
+        if (registros.length === 0) {
+            detalle.push(
+                {
+                    "productoId": 1,
+                    "alumnoId": id,
+                    "monto": fijos.find(prod => prod.idProducto === 1)?.monto,
+                    "dias": "string"
+                },
+                {
+                    "productoId": 2,
+                    "alumnoId": id,
+                    "monto": fijos.find(prod => prod.idProducto === 2)?.monto,
+                    "dias": "string"
+                },
+                {
+                    "productoId": 3,
+                    "alumnoId": id,
+                    "monto": fijos.find(prod => prod.idProducto === 3)?.monto,
+                    "dias": "string"
+                },
+            );
+            setRegistros(prevRegistros => [...prevRegistros, ...detalle]);
+
+        } else {
+            let validado = registros.filter(reg => reg.alumnoId === id);
+            if (validado.length === 0) {
+                console.log("entra al push 2");
+                detalle.push(
+                    {
+                        "productoId": 1,
+                        "alumnoId": id,
+                        "monto": fijos.find(prod => prod.idProducto === 1)?.monto,
+                        "dias": "string"
+                    },
+                    {
+                        "productoId": 2,
+                        "alumnoId": id,
+                        "monto": fijos.find(prod => prod.idProducto === 2)?.monto,
+                        "dias": "string"
+                    },
+                    {
+                        "productoId": 3,
+                        "alumnoId": id,
+                        "monto": fijos.find(prod => prod.idProducto === 3)?.monto,
+                        "dias": "string"
+                    },
+                );
+                console.log(detalle);
+                setRegistros(prevRegistros => [...prevRegistros, ...detalle]);
+
+            } else {
+                validado = registros.filter(reg => reg.alumnoId != id);
+                if (validado.length > 0) {
+                    setRegistros(validado);
+                } else {
+                    setRegistros([]);
+                }
+            }
+        }
     }
 
+    const alumnoActivo = (id) => {
+        const validacionAlumno = registros.filter(reg => reg.alumnoId != id);
+        if (validacionAlumno.length === 0) {
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
+    const horario = (id, idHorario) => {
+        let reg = {
+            "productoId": parseInt(idHorario),
+            "alumnoId": id,
+            "monto": mensuales.find(prod => prod.idProducto === parseInt(idHorario))?.monto,
+            "dias": "string"
+        }
+        setRegistros(prevRegistros => [...prevRegistros, reg]);
+      
+    }
     //UseEffect*******************************************************
 
     useEffect(() => {
@@ -94,6 +172,10 @@ export const Matricula2 = () => {
         cargarFijos();
         cargarMensuales();
     }, []);
+
+    useEffect(() => {
+        console.log(registros);
+    }, [registros]);
 
 
     return (
@@ -109,6 +191,7 @@ export const Matricula2 = () => {
                                     <select
                                         onChange={(e) => handleUseSelect(e.target.value)}
                                         value={usSelect.idUsuario || "0"}>
+
                                         <option value="0"> Seleccione un padre </option>
                                         {usuarios.map((use) => (
                                             <option key={use.idUsuario} value={use.idUsuario}> {use.nombreUsuario} {use.apellidosUsuario}</option>
@@ -124,17 +207,34 @@ export const Matricula2 = () => {
 
                             {usSelect.alumnos && usSelect.alumnos.length > 0 && (
                                 <div className="hijos" >
-                                    <label>Hijos</label> 
+                                    <label>Hijos</label>
                                     {usSelect.alumnos.map((hijo, index) => (
-                                        <div key={index}>                                            
-                                            <label key={index}>
-                                                <input
-                                                    type="checkbox"
-                                                    key={hijo.idAlumno}
-                                                    onChange={() => handleCheck(index, hijo.idAlumno)}
-                                                />
-                                                {hijo.nombreAlumno}
-                                            </label>
+                                        <div key={index}>
+                                            <div>
+                                                <label key={index}>
+                                                    <input
+                                                        type="checkbox"
+                                                        key={hijo.idAlumno}
+                                                        onChange={() => handleCheck(hijo.idAlumno)}
+                                                    />
+
+                                                    {hijo.nombreAlumno}
+                                                </label>
+
+                                                {alumnoActivo(hijo.idAlumno) && (
+                                                    <select
+                                                        key={index}
+                                                        onChange={(e) => horario(hijo.idAlumno, e.target.value)}
+                                                        value={(e) => registros.find((pr) => pr.idProducto === e.target.value)?.productoId || "0"}
+                                                    >
+                                                        <option value="0"> Seleccione un horario</option>
+                                                        {mensuales.map((ho) => (
+                                                            <option key={ho.idProducto} value={ho.idProducto}>{ho.nombreProducto}</option>
+                                                        ))}
+                                                    </select>
+                                                )}
+                                            </div>
+
                                         </div>
                                     ))}
                                 </div>
